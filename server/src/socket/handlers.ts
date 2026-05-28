d6d75d44e6be7380245410b254ad0ef847944c75
Prince Mishra <princemishra95927@gmail.com>
2026-05-28 10:32:53 +0530

    feat: implement temporary user sessions, private rooms, and realtime messaging

--- server/src/socket/handlers.ts
+++ server/src/socket/handlers.ts
@@ -1,5 +1,6 @@
 import { Socket } from "socket.io";
-import { addUser, isUsernameTaken, removeUser } from "../users/userStore";
+import { addUser, getUser, isUsernameTaken, removeUser } from "../users/userStore";
+import { addUserToRoom, createRoom, getRoom } from "../rooms/roomStore";
 
 export const registerSocketHandlers = (socket: Socket) => {
     console.log("User connected:", socket.id);
@@ -22,14 +23,78 @@ export const registerSocketHandlers = (socket: Socket) => {
 
         addUser({
             socketId: socket.id,
-            username,
+            username: trimmedUsername,
             joinedAt: Date.now()
         })
 
         socket.emit("username_success", trimmedUsername)
 
-        console.log(`${trimmedUsername} joined`);
+    })
 
+    socket.on("create_room", (roomId: string) => {
+        const existingRoom = getRoom(roomId);
+
+        if (existingRoom) {
+            socket.emit(
+                "room_error",
+                "Room already exists"
+            )
+            return
+        }
+
+        createRoom(roomId)
+
+        const joined = addUserToRoom(
+            roomId,
+            socket.id
+        )
+
+        if (!joined) {
+            socket.emit(
+                "room_error",
+                "Unable to join room"
+            )
+            return
+        }
+
+        socket.join(roomId)
+
+        socket.emit("room_created", roomId)
+
+    })
+
+    socket.on("join_room", (roomId: string) => {
+        const room = getRoom(roomId)
+
+        if (!room) {
+            socket.emit("room_error", "Room not found")
+            return
+        }
+
+        const joined = addUserToRoom(roomId, socket.id)
+        if (!joined) {
+            socket.emit("room_error", "Room Full")
+            return
+        }
+
+        socket.join(roomId)
+
+        socket.emit("room_joined", roomId)
+
+        socket.to(roomId).emit("user_joined")
+
+
+    })
+
+    socket.on("send_message", ({ roomId, message }) => {
+        const user = getUser(socket.id)
+        if (!user) return
+
+        socket.to(roomId).emit("receive_message", {
+            username: user.username,
+            message,
+            timestamp: Date.now()
+        })
     })
 
     socket.on("disconnect", () => {
